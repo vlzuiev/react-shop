ca65b83e547b84e5656e78ad7d56b7d45327aaa7
Vladyslav Zuiev <vladyslavzuiev@Vladyslavs-MacBook-Pro.local>
2019-12-05 19:07:57 +0100

    adding local storage with redux persist library

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react'
+
 import './index.css';
 import App from './App';
-import store from './redux/store'
+import {store, persistor} from './redux/store'
 
 ReactDOM.render( 
     <Provider store={store} >
         <BrowserRouter>
+            <PersistGate persistor={persistor}/> 
             <App />
         </BrowserRouter> 
     </Provider>
