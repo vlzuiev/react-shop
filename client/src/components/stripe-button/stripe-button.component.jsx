77ab3434b91b33b6e1621e0858ca49ffc3d3f7c0
Vladyslav Zuiev <user.darky@gmail.com>
2020-03-20 18:39:33 +0100

    Separating client side from back end side

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import StripeCheckout from 'react-stripe-checkout';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price, history }) => {
+    const currentUser = useSelector(selectCurrentUser);
+
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_A0v5iG1WXz4n3RCfV8uCE2gD00CpAFKE9n';
+
+    const onToken = token => {
+        axios({
+            url:'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment was Succesful');
+        }).catch(err => {
+            console.log('Payment err: ', JSON.parse(err))
+            alert("There was an issue with your credit cart. Please make sure you use the provided credit card")
+        });
+    }
+
+    return (
+        currentUser ? <StripeCheckout
+            label='Pay Now'
+            name="CRWN Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay now'
+            token={onToken}
+            stripeKey={publishableKey}>
+            <button className="btn btn-primary">
+                Pay Now
+                </button>
+        </StripeCheckout> : <button onClick={() => history.push('/signin')} className="btn btn-primary">
+                Pay Now
+                </button>
+    );
+
+};
+
+export default withRouter(StripeCheckoutButton);
